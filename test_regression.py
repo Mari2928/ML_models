4f53e1f65416dbc71b644a748d05f3a84bafa048
dora2928 <mari@turintech.ai>
2025-02-26 12:30:13 +0000

    Add save variable feature to test suite.

--- test_regression.py
+++ test_regression.py
@@ -1,12 +1,31 @@
 import regression
 from pathlib import Path
+import numpy as np
+import os
 
 result = regression.mse_manual
 weights = regression.manual_weights
 
 output = Path("/home/mari.ashiga/ml_results.txt")
+output_all = Path("/home/mari.ashiga/all_results.npy")
+
+# log output to be grabbed by Artemis
 with open(output, 'a') as f:
     f.write(f"Weights: {weights}; MSE: {result}\n")
 full_output = output.read_text()
-
 print(full_output)
+
+# save all results
+if os.path.exists(output_all):
+    all_results= np.load(output_all, allow_pickle=True).item()
+else:
+    all_results = {}
+    all_results["weights"] = []
+    all_results["mses"]= []
+all_results["weights"].append(weights)
+all_results["mses"].append(result)
+with open(output_all, "wb") as f:
+    np.save(f, all_results)
+
+
+
